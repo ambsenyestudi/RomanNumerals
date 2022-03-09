6df9455600d5b5c137bd511138221e7a4bad2175
ambsenyestudi <adrifre7@hotmail.com>
2022-03-09 06:55:31 +0100

    2 units red

--- RomanNumerals/RomanNumerals.Test/RomanNumeralsShould.cs
+++ RomanNumerals/RomanNumerals.Test/RomanNumeralsShould.cs
@@ -6,12 +6,13 @@ namespace RomanNumerals.Test
 {
     public class RomanNumeralsShould
     {
-        [Fact]
-        public void HaveUnits()
+        [Theory]
+        [InlineData(1,"I")]
+        [InlineData(2, "II")]
+        public void HaveUnits(int arabic, string expected)
         {
-            var exptected = "I";
-            var sut = new RomanNumeral(1);
-            Assert.Equal(exptected, sut.ToString());
+            var sut = new RomanNumeral(arabic);
+            Assert.Equal(expected, sut.ToString());
         }
     }
 }

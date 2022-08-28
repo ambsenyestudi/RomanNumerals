e91603d044679a0cd2228c2d83148bfafe81e0f9
ambsenyestudi <adrifre7@hotmail.com>
2022-08-28 18:45:48 +0200

    Test many tokens

--- test/RomanNumeralShould.cs
+++ test/RomanNumeralShould.cs
@@ -6,6 +6,12 @@ public class RomanNumeralShould
 {
     [Theory]
     [InlineData(1,"I")]
+    [InlineData(5, "V")]
+    [InlineData(10, "X")]
+    [InlineData(50, "L")]
+    [InlineData(100, "C")]
+    [InlineData(500, "D")]
+    [InlineData(1000, "M")]
     public void Convert_When_Token(int arabic, string expected)
     {
         var romanNumeral = new RomanNumeral(arabic);
